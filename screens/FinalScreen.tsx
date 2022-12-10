ede25a14dafae54bae5bd568eb44b333f39ba345
marcelog5 <marcelovictorg2@gmail.com>
2022-12-10 18:40:00 -0300

    Clean files

---
+++ screens/FinalScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <p>FInal</p>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
